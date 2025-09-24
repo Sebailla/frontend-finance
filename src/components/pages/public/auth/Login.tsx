3ec63d56df7ebf18f6599aa9a83d256d15d45185
Sebailla <seba.illa.1977@gmail.com>
2025-09-24 16:16:42 -0300

    fix: correct validation message for last name in RegisterSchema

--- src/components/pages/public/auth/Login.tsx
+++ src/components/pages/public/auth/Login.tsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 
 const Login = () => {
     return (
-        <div className="relative z-50 flex w-auto md:w-96 pt-10 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-lg">
+        <div className="relative z-50 flex w-auto md:w-96 mt-10 md:mt-20 lg:mt-0 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-lg">
 
             <HeaderCard 
             title={'Sign In'} 
--- src/domain/schemas/index.ts
+++ src/domain/schemas/index.ts
@@ -2,7 +2,7 @@ import { z } from "zod"
 
 export const RegisterSchema = z.object({
     name: z.string().min(3, { message: 'Name must be at least 3 characters long' }),
-    lastName: z.string().min(3, { message: 'Name must be at least 3 characters long' }),
+    lastName: z.string().min(3, { message: 'Lastname must be at least 3 characters long' }),
     email: z.string().min(1, { message: 'Invalid email address' }).email({ message: 'Invalid email address' }),
     password: z.string().min(8, { message: 'Password must be at least 8 characters long' }),
     confirmPassword: z.string(),
